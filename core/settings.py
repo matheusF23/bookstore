5d3abcc9b0c88d2713605e006d0eab818aaf79b5
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2021-04-27 13:00:21 -0300

    feat: created templates

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # My apps
     'apps.accounts',
+    'apps.pages',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
