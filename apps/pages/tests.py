8f596ebb51b519d5e6235a9b01e02e1f41791b34
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2021-04-29 10:41:04 -0300

    test: testing html

--- apps/pages/tests.py
+++ apps/pages/tests.py
@@ -15,3 +15,12 @@ class HomepageTests(SimpleTestCase):
     def test_homepage_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_homepage_contains_correct_htmml(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Homepage')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        response = self.client.get('/')
+        self.assertNotContains(
+            response, 'Hi there! I should not be on the page.')
